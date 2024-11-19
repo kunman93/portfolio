ec42113be97ad56c9f73e6a3114bad9c42f3cea2
kunman93 <kun.manu93@gmail.com>
2024-11-19 11:53:53 +0100

    feat: implement a bare base HomeComponent

---
+++ src/app/feature/home/home.component.spec.ts
@@ -0,0 +1,35 @@
+import { Shallow } from 'shallow-render';
+import { HomeComponent } from './home.component';
+import { FeatureModule } from '../feature.module';
+import { WorkstationComponent } from './workstation.component';
+
+describe("HomeComponent", () => {
+    let shallow: Shallow<HomeComponent>;
+
+    beforeEach(() => {
+        shallow = new Shallow(HomeComponent, FeatureModule);
+    });
+
+    it("creates a component", async () => {
+        const { find } = await shallow.render(`<app-home></app-home>`);
+        expect(find).toBeTruthy();
+    });
+
+    describe("template", () => {
+        it("displays title and description", async () => {
+            const { find } = await shallow.render(`<app-home></app-home>`);
+            const title = find("#title");
+            const description = find("#description");
+
+            expect(title.nativeElement.textContent).toBe("Hi, I'm Manu");
+            expect(description.nativeElement.textContent).toBe("I'm a Full-Stack Software Engineer");
+        });
+
+        it("displays the workstation", async () => {
+            const { findComponent } = await shallow.render(`<app-home></app-home>`);
+            const workstationComponent = findComponent(WorkstationComponent);
+
+            expect(workstationComponent).toBeTruthy();
+        });
+    });
+});
---
+++ src/app/feature/home/workstation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workstation',
+  templateUrl: './workstation.component.html',
+  styleUrl: './workstation.component.scss'
+})
+export class WorkstationComponent {
+
+}
