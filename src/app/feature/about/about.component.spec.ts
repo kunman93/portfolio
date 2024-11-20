a23f8d04b9af2fa81c5f73047c3d795e8acb855a
kunman93 <kun.manu93@gmail.com>
2024-11-20 19:24:15 +0100

    feat: display overview section
    - fix styling in `home.component.html`
    - display small title, title and overview text

--- src/app/feature/about/about.component.spec.ts
+++ src/app/feature/about/about.component.spec.ts
@@ -1,23 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Shallow } from 'shallow-render';
 import { AboutComponent } from './about.component';
+import { FeatureModule } from '../feature.module';
 
 describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+    let shallow: Shallow<AboutComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AboutComponent]
-    })
-    .compileComponents();
+    beforeEach(() => {
+        shallow = new Shallow(AboutComponent, FeatureModule);
+    });
 
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('creates a component', async () => {
+        const { find } = await shallow.render(`<app-about></app-about>`);
+        expect(find).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    describe('template', () => {
+        it('displays the small title', async () => {
+            const { find } = await shallow.render(`<app-about></app-about>`);
+            const smallTitle = find('#smallTitle');
+
+            expect(smallTitle.nativeElement.textContent).toBe('INTRODUCTION');
+        });
+
+        it('displays the overview title', async () => {
+            const { find } = await shallow.render(`<app-about></app-about>`);
+            const title = find('#title');
+
+            expect(title.nativeElement.textContent).toBe('Overview.');
+        });
+
+        it('displays the overview text', async () => {
+            const { find } = await shallow.render(`<app-about></app-about>`);
+            const overviewText = find('#overviewText');
+            const expectedOverviewText = "I am a Full-Stack Software Engineer with hands-on experience in developing and testing scalable web applications within an agile team consisting of Software Engineers, UI / UX Designers and Business Analysts. With a solid foundation in software development, combined with a passion for continuous learning, alongside working with a cohort of experienced professionals who share my dedication and enthusiasm, I'm eager to contribute to your innovative team and help drive the success of your company.";
+
+            expect(overviewText.nativeElement.textContent.trim()).toBe(expectedOverviewText);
+        });
+    });
 });
