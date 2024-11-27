a3f98f384716f815e9110fc2e68e07626101a9df
kunman93 <kun.manu93@gmail.com>
2024-11-27 16:24:08 +0100

    feat: display small title, title and text
    - display small title, tilte and text in Projects section

--- src/app/feature/work/components/project.component.spec.ts
+++ src/app/feature/work/components/project.component.spec.ts
@@ -13,4 +13,28 @@ describe('ProjectComponent', () => {
         const { find } = await shallow.render(`<app-project></app-project>`);
         expect(find).toBeTruthy();
     });
+
+    describe('template', () => {
+        it('displays the small title', async () => {
+            const { find } = await shallow.render(`<app-project></app-project>`);
+            const smallTitle = find('#smallTitle');
+
+            expect(smallTitle.nativeElement.textContent).toBe('MY WORK');
+        });
+
+        it('displays the projects title', async () => {
+            const { find } = await shallow.render(`<app-project></app-project>`);
+            const title = find('#title');
+
+            expect(title.nativeElement.textContent).toBe('Projects.');
+        });
+
+        it('displays the projects text', async () => {
+            const { find } = await shallow.render(`<app-project></app-project>`);
+            const overviewText = find('#projectsText');
+            const expectedProjectsText = "The projects below showcase my skills and experience. Each one includes a brief description and a link to my GitHub repository, where you can explore live demos. These projects demonstrate my expertise in solving problems, utilizing various technologies, and managing projects efficiently.";
+
+            expect(overviewText.nativeElement.textContent.trim()).toBe(expectedProjectsText.trim());
+        });
+    });
 });
