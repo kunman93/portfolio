6edebcd81754813f989d9db2bae5fbafaa5c3a4f
kunman93 <kun.manu93@gmail.com>
2024-12-09 19:18:05 +0100

    refactor: rename folder and timer service

--- src/app/core/notification/services/toast.service.ts
+++ src/app/core/notification/services/toast.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ToastData } from '../models/toast-data';
 import { TOASTER_CONFIG } from 'src/app/configs/toaster.config';
-import { TimerService } from 'src/app/shared/services/timer.service';
+import { Timer } from 'src/app/shared/utils/timer';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ToastService {
     private toastDatas: ToastData[] = [];
-    private timerService = new TimerService(() => this.remove(0), TOASTER_CONFIG.DURATION);
+    private timer = new Timer(() => this.remove(0), TOASTER_CONFIG.DURATION);
 
     add(toastData: ToastData): void {
         this.toastDatas.push(toastData);
-        this.timerService.reset();
+        this.timer.reset();
 
         if (this.toastDatas.length > 1) {
             this.remove(0);
