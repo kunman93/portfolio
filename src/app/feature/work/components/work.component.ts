2fc9f28005be06db7d5165e35fc9dff0fa6590db
kunman93 <kun.manu93@gmail.com>
2024-12-11 16:38:35 +0100

    feat: animate academic history section

--- src/app/feature/work/components/work.component.ts
+++ src/app/feature/work/components/work.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, NgZone } from '@angular/core';
 import { academicHistory } from '../data/academic-history-data';
 import { workHistory } from '../data/work-history-data';
+import { History } from "../models/history";
 import { GsapAnimationService } from 'src/app/core/services/gsap-animation.service';
 
 @Component({
@@ -20,52 +21,12 @@ export class WorkComponent implements AfterViewInit {
     ngAfterViewInit(): void {
         this.zone.runOutsideAngular(() => {
             this.animateExperienceSection();
+            this.animateAcademicHistorySection();
         });
     }
 
     private animateExperienceSection(): void {
-        this.gsapAnimationService.animateSmallTitleAndTitle(
-            "#experienceSmallTitle, #experienceTitle",
-            "#workExperience #timelineContainer"
-        );
-
-        this.workHistory.forEach((_value, idx) => {
-            // ---- Mobile ----
-            this.gsapAnimationService.animateTimelineIcon(
-                `#workExperience #logoContainerMobile${idx}`,
-                `#workExperience #logoContainerMobile${idx}`
-            );
-
-            this.gsapAnimationService.animateExperienceCard(
-                `#workExperience #experienceCardMobile${idx}`,
-                `#workExperience #logoContainerMobile${idx}`,
-                100
-            );
-
-            this.gsapAnimationService.animateWorkingPeriod(
-                `#workExperience #workingPeriodMobile${idx}`,
-                `#workExperience #workingPeriodMobile${idx}`,
-                100
-            );
-
-            // ---- Desktop ----
-            this.gsapAnimationService.animateTimelineIcon(
-                `#workExperience #logoContainerDesktop${idx}`,
-                `#workExperience #logoContainerDesktop${idx}`
-            );
-
-            this.gsapAnimationService.animateExperienceCard(
-                `#workExperience #experienceCardDesktop${idx}`,
-                `#workExperience #logoContainerDesktop${idx}`,
-                idx % 2 === 0 ? -100 : 100
-            );
-
-            this.gsapAnimationService.animateWorkingPeriod(
-                `#workExperience #workingPeriodDesktop${idx}`,
-                `#workExperience #workingPeriodDesktop${idx}`,
-                idx % 2 === 0 ? 100 : -100
-            );
-        });
+        this.animateWorkExperienceOrAcademicHistorySection("#workExperience", this.workHistory);
 
         this.gsapAnimationService.gsap.from("app-employment-reference", {
             opacity: 0,
@@ -76,6 +37,55 @@ export class WorkComponent implements AfterViewInit {
                 scrub: 4,
                 end: "top 100%"
             }
+        });
+    }
+
+    private animateAcademicHistorySection(): void {
+        this.animateWorkExperienceOrAcademicHistorySection("#academicHistory", this.academicHistory);
+    }
+
+    private animateWorkExperienceOrAcademicHistorySection(sectionId: string, history: History[]) {
+        this.gsapAnimationService.animateSmallTitleAndTitle(
+            `${sectionId} #experienceSmallTitle, ${sectionId} #experienceTitle`,
+            `${sectionId} #timelineContainer`
+        );
+
+        history.forEach((_value, idx) => {
+            // ---- Mobile ----
+            this.gsapAnimationService.animateTimelineIcon(
+                `${sectionId} #logoContainerMobile${idx}`,
+                `${sectionId} #logoContainerMobile${idx}`
+            );
+
+            this.gsapAnimationService.animateExperienceCard(
+                `${sectionId} #experienceCardMobile${idx}`,
+                `${sectionId} #logoContainerMobile${idx}`,
+                100
+            );
+
+            this.gsapAnimationService.animateWorkingPeriod(
+                `${sectionId} #workingPeriodMobile${idx}`,
+                `${sectionId} #workingPeriodMobile${idx}`,
+                100
+            );
+
+            // ---- Desktop ----
+            this.gsapAnimationService.animateTimelineIcon(
+                `${sectionId} #logoContainerDesktop${idx}`,
+                `${sectionId} #logoContainerDesktop${idx}`
+            );
+
+            this.gsapAnimationService.animateExperienceCard(
+                `${sectionId} #experienceCardDesktop${idx}`,
+                `${sectionId} #logoContainerDesktop${idx}`,
+                idx % 2 === 0 ? -100 : 100
+            );
+
+            this.gsapAnimationService.animateWorkingPeriod(
+                `${sectionId} #workingPeriodDesktop${idx}`,
+                `${sectionId} #workingPeriodDesktop${idx}`,
+                idx % 2 === 0 ? 100 : -100
+            );
         });
     }
 }
