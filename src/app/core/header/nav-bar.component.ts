41c0ada7f9dcb793a16e3d72954d326340f547b3
kunman93 <kun.manu93@gmail.com>
2024-12-14 19:12:02 +0100

    feat: animate dropdown options

--- src/app/core/header/nav-bar.component.ts
+++ src/app/core/header/nav-bar.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, QueryList, ViewChildren } from '@angular/core';
 import { NOJI_LOGO } from 'assets/assets.constants';
+import { GsapAnimationService } from '../services/gsap-animation.service';
 
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
     readonly NOJI_LOGO = NOJI_LOGO;
     isSelected = false;
+
+    @ViewChildren('dropDownOptionContainerRef', { read: ElementRef }) dropDownOptionContainerRef!: QueryList<ElementRef>;
+
+    constructor(
+        private zone: NgZone,
+        private gsapAnimationService: GsapAnimationService
+    ) { }
+
+    ngAfterViewInit(): void {
+        this.dropDownOptionContainerRef.changes.subscribe((dropDownOptionContainer: QueryList<ElementRef>) => {
+            if (dropDownOptionContainer.length > 0) {
+                this.zone.runOutsideAngular(() => {
+                    this.gsapAnimationService.gsap.from(`#${dropDownOptionContainer.first.nativeElement.id}`, {
+                        opacity: 0,
+                        skewY:'30deg', 
+                        y: -100,
+                        duration: 1,
+                        ease: "back.inOut(4)"
+                    });
+                });
+            }
+        });
+    }
 
     onDropdownMenuClick(): void {
         this.isSelected = !this.isSelected;
