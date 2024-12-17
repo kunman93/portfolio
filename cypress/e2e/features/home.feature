8aba3f0e0d0c70a88a143f5afd12a378e2f359fa
kunman93 <kun.manu93@gmail.com>
2024-12-17 21:32:02 +0100

    test: implement navigation e2e tests

--- cypress/e2e/features/home.feature
+++
@@ -1,4 +0,0 @@
-Feature: Home
-  Scenario: visiting the portfolio
-    When I visit the portfolio
-    Then I should see a title
--- cypress/support/step_definitions/home.ts
+++
@@ -1,9 +0,0 @@
-import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
-
-When("I visit the portfolio", () => {
-    cy.visit("/");
-});
-
-Then("I should see a title", () => {
-    cy.get("#homeTitle").contains("Hi, I'm Manu");
-});
