be8cc157d8fe0f28daf1a80f163e51326d02ccd3
kunman93 <kun.manu93@gmail.com>
2024-12-17 09:58:24 +0100

    fix: unsubscribe Subject to prevent memory leaks

--- src/app/core/header/nav-bar.component.ts
+++ src/app/core/header/nav-bar.component.ts
@@ -1,19 +1,26 @@
-import { AfterViewInit, Component, ElementRef, NgZone, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnDestroy, QueryList, ViewChildren } from '@angular/core';
 import { NOJI_LOGO } from 'assets/assets.constants';
+import { Image } from 'shared/models/image'
 import { GsapAnimationService } from '../services/gsap-animation.service';
-import { Subject } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent implements AfterViewInit {
-    readonly NOJI_LOGO = NOJI_LOGO;
+export class NavBarComponent implements AfterViewInit, OnDestroy {
+    readonly logo: Image = {
+        srcImage: NOJI_LOGO,
+        alt: "Noji Logo"
+    };
+
     isSelected = false;
 
-    private isSelectedSubject = new Subject<boolean>();
+    private isSelectedSubject$ = new Subject<boolean>();
+    private unsubscribe$: Subject<void> = new Subject();
+
 
     @ViewChildren('dropDownOptionContainerRef', { read: ElementRef })
     private dropDownOptionContainerRef!: QueryList<ElementRef>;
@@ -40,19 +47,23 @@ export class NavBarComponent implements AfterViewInit {
         });
 
         if (!!this.dropDownOptionContainerRef) {
-            this.isSelectedSubject.subscribe((isSelected) => {
-                this.zone.runOutsideAngular(() => {
-                    this.gsapAnimationService.gsap.to(`#${this.dropDownOptionContainerRef.first.nativeElement.id}`, {
-                        opacity: 0,
-                        skewY: '30deg',
-                        y: -100,
-                        duration: 1,
-                        ease: "back.inOut(4)"
-                    }).then(() => {
-                        this.zone.run(() => this.isSelected = isSelected)
+            this.isSelectedSubject$
+                .pipe(
+                    takeUntil(this.unsubscribe$)
+                )
+                .subscribe((isSelected) => {
+                    this.zone.runOutsideAngular(() => {
+                        this.gsapAnimationService.gsap.to(`#${this.dropDownOptionContainerRef.first.nativeElement.id}`, {
+                            opacity: 0,
+                            skewY: '30deg',
+                            y: -100,
+                            duration: 1,
+                            ease: "back.inOut(4)"
+                        }).then(() => {
+                            this.zone.run(() => this.isSelected = isSelected)
+                        });
                     });
                 });
-            });
         }
     }
 
@@ -60,11 +71,16 @@ export class NavBarComponent implements AfterViewInit {
         if (!this.isSelected) {
             this.isSelected = true;
         } else {
-            this.isSelectedSubject.next(false);
+            this.isSelectedSubject$.next(false);
         }
     }
 
     onLogoNameOccupationClick(): void {
         this.router.navigate(['/']);
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 }
