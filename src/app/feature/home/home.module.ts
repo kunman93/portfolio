ae4a0ffd48648ed4468ac115f4a43053a482a041
kunman93 <kun.manu93@gmail.com>
2024-12-19 16:18:42 +0100

    feat: remove `WorkStationComponent`

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { WorkstationComponent } from './workstation.component';
 import { SharedModule } from "../../shared/shared.module";
 
 
@@ -10,7 +9,6 @@ import { SharedModule } from "../../shared/shared.module";
 @NgModule({
     declarations: [
         HomeComponent,
-        WorkstationComponent,
     ],
     imports: [
         CommonModule,
